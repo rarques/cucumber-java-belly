77014eda9a514eec118017e5e87836316e593676
Roger Arqués Vall <rarquesvall@gmail.com>
2017-09-14 15:44:46 +0200

    Belly implementation finished

--- src/main/java/mybelly/Belly.java
+++ src/main/java/mybelly/Belly.java
@@ -5,9 +5,15 @@ public class Belly {
     private int cukes;
 
     public void eatCukes(int cukes) {
+        this.cukes = cukes;
     }
 
     public String getSound(int waitingTime) {
-        return "";
+        if (cukes <= 20)
+            return "silent";
+        else if (waitingTime >= 1)
+            return "growl";
+        else
+            return "silent";
     }
 }
--- src/test/java/mybelly/steps/BellyCukesSteps.java
+++ src/test/java/mybelly/steps/BellyCukesSteps.java
@@ -1,7 +1,9 @@
 package mybelly.steps;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import mybelly.Belly;
 
 import static org.hamcrest.core.Is.is;
@@ -12,14 +14,24 @@ public class BellyCukesSteps {
     private int waitingTime;
 
     @Given("^I have (\\d+) (?:cuke|cukes) in my belly$")
-    public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
+    public void iHaveCukesInMyBelly(int cukes) throws Throwable {
         belly = new Belly();
         belly.eatCukes(cukes);
     }
 
     @Then("^My belly should (?:be )?\"([^\"]*)\"$")
-    public void my_belly_should(String expectedSound) throws Throwable {
+    public void myBellyShould(String expectedSound) throws Throwable {
         String actualSound = belly.getSound(waitingTime);
         assertThat(actualSound, is(expectedSound));
+    }
+
+    @When("^I don't wait$")
+    public void iDonTWait() throws Throwable {
+        belly.getSound(0);
+    }
+
+    @When("^I wait (\\d+) hour$")
+    public void iWaitHour(int waitingTime) throws Throwable {
+        this.waitingTime = waitingTime;
     }
 }
